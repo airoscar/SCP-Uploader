eb3a262ecf2ed53d583f368024131c66f678376f
airoscar <oscar.y.chen@gmail.com>
2019-10-01 16:06:26 -0600

    updated work in progress

--- uploader.sh
+++ uploader.sh
@@ -1,64 +1,78 @@
 #!/bin/bash 
 
-remoteServer="wgmsrv.ucalgary.ca"
-
-function upload {
-	
-	destination = "$1@${remoteServer}:~/$2"
-	localDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-	
-	scp -r $1
-}
-
 echo "#############################"
 echo "##### Auto File Uploader ####"
 echo "##### wgmsrv.ucalgary.ca ####"
 echo "#############################"
 
-echo "Set upload source:"
-OPTIONS=("All files in current directory" 
-	# "Select a file from current directory" 
-	"Specify a path")
-select opt in "${OPTIONS[@]}" 
-do
-	case $opt in
-		"All files in current directory")
-			source="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-			echo "current directory selected: ${source}"
-			break
-			;;
-		# "Select a file from current directory")
-		# 	sourceRoot="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-		# 	echo "Select one of the following files: "
-		# 		for entry in "$sourceRoot"/*
-		# 		do
-		# 			echo "$entry"
-		# 		done
-		# 	break
-		# 	;;
+currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+scriptFileName="${BASH_ARGV[0]}"
+
+function setUser {
+	echo "Enter username (ie: firstname.lastname)"
+	read username
+
+	echo "Please enter password."
+	read -s password
+}
+
+function setServer {
+	echo "Enter server address (Default: wgmsrv.ucalgary.ca)"
+	read remoteServer
+	if [[ -z "$remoteServer" ]]; then
+		remoteServer="wgmsrv.ucalgary.ca"
+	fi
+}
+
+function setSource {
+
+	echo "Set up source:"
+	OPTIONS=("All files in current directory" 
 		"Specify a path")
-			echo "Enter a source path: "
-			read source
-			break
-			;;
-		*)
-			echo "Invalid selection"
-	esac
-done
+	select opt in "${OPTIONS[@]}" 
+	do
+		case $opt in
+			"All files in current directory")
+				source=$currentDir
+				break
+				;;
 
+			"Specify a path")
+				echo "Enter a source path: "
+				read source
+				break
+				;;
+			*)
+				echo "Invalid selection"
+		esac
+	done
+}
 
-echo "Enter username (ie: firstname.lastname)"
-read username
+function setDestination {
+	echo "Enter destination path: (Default: ~/)"
+	read destinationFolder
 
-echo "Please enter password."
-read -s password
+	if [[ -z "$destinationFolder" ]]; then
+		destinationFolder="~/"
+	fi
 
-echo "Enter destination path relative to home folder: (ie: my_project/data)"
-read destinationFolder
-destination="${username}@${remoteServer}:~/${destinationFolder}"
+	destination="${username}@${remoteServer}:${destinationFolder}"
+}
 
+function upload {
+	uploadFrom=$source
+	uploadTo=$destination
+	command="rsync -avz -e ssh --exclude \"${scriptFileName}\" --exclude \"*.git\" ${uploadFrom} ${uploadTo}"
+	echo $command
+	eval $command
+	
+}
+
+setServer
+setUser
+setSource
+setDestination
 echo "Auto uploader has been set up to upload from ${source} to ${destination}, press any key to upload, press ESC to exit."
-
 
 while :
 do
@@ -74,4 +88,6 @@ do
 			upload $source $destination $password
 			;;
 	esac
+
+	echo "Press any key to sync again, ESC to exit.."
 done
